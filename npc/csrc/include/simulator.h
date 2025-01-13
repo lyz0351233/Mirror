4fd16e3154238e832d03760dd6cbc18ff2c8bf01
boss <boss@qq.com>
2025-01-13 19:42:13 +0800

    支持addi与ebreak的npc

--- npc/csrc/include/simulator.h
+++ npc/csrc/include/simulator.h
@@ -4,6 +4,13 @@
 #include "verilated_fst_c.h"
 #include <iostream>
 #include <verilated.h>
+#include <cpu.h>
+#include <args.h>
+#include <Vtop___024root.h>
+enum class SIM_STATE{
+  NORMAL,
+  QUIT
+};
 class Simulator {
 private:
   TOP_NAME *top;
@@ -15,9 +22,17 @@ private:
   void step_and_dump_wave(); 
   void single_cycle();
   void args_init(int argc, char *argv[]);
+  void cpu_update(){
+    for (int i = 0; i < cpu.nums; i++) {
+      cpu.gpr[i] = top->rootp->top__DOT__mreg__DOT__rf[i];
+    }
+    cpu.pc = top->rootp->top__DOT__pc;
+  }
+  SIM_STATE state = SIM_STATE::NORMAL;
 public:
+  Cpu<32> cpu;
   
-  Simulator(int argc, char* argv[]);
+  Simulator(Args& args);
   ~Simulator();
   int run();
   //重置n个时钟周期
@@ -26,6 +41,10 @@ public:
     while (n-- > 0)
       single_cycle();
     top->reset = 0;
+    cpu_update();
   }
-  int exec_once();
+  SIM_STATE exec_once();
+  void quit(){
+    state = SIM_STATE::QUIT;
+  }
 };
--- npc/csrc/src/simulator.cpp
+++ npc/csrc/src/simulator.cpp
@@ -1,9 +1,8 @@
 #include "simulator.h"
 #include <getopt.h>
 #include <nvboard.h>
-Simulator::Simulator(int argc, char* argv[]){
+Simulator::Simulator(Args& args) :is_nvboard(args.is_nvboard), is_gtk(args.is_gtk), gtk_file(args.gtk_file){
   top = new TOP_NAME;
-  args_init(argc, argv);
   if (is_gtk) {
     Verilated::traceEverOn(true);
     contextp = new VerilatedContext;
@@ -18,26 +17,6 @@ Simulator::Simulator(int argc, char* argv[]){
   }
 }
 
-void Simulator::args_init(int argc, char* argv[]){
-  const struct option table[] = {
-    {"nvboard"  , no_argument      , NULL, 'n'},
-    {"gtktrace" , required_argument, NULL, 'g'},
-    {0          , 0                , NULL,  0 },
-  };
-  int o;
-  while ( (o = getopt_long(argc, argv, "ng:", table, NULL)) != -1) {
-    switch (o) {
-      case 'n': is_nvboard = true; break;
-      case 'g': is_gtk = true; gtk_file = optarg; break;
-      default:
-        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-        printf("\t-n,--nvboard            run with nvboard\n");
-        printf("\t-g,--gtk=FILE           run with gtk_trace FILE\n");
-        printf("\n");
-        exit(0);
-    }
-  }
-}
 
 void Simulator::step_and_dump_wave() {
     top->eval();
@@ -45,22 +24,24 @@ void Simulator::step_and_dump_wave() {
       contextp->timeInc(1);
       tfp->dump(contextp->time());
     }
-}
+  }
 
 void Simulator::single_cycle() {
     top->clock = 1;
     step_and_dump_wave();
     top->clock = 0;
     step_and_dump_wave();
-}
+  }
 
-int Simulator::exec_once(){
+SIM_STATE Simulator::exec_once(){
   if (is_nvboard) nvboard_update();
   single_cycle();
-  return 0;
+  cpu_update();
+  return state;
 }
 
 int Simulator::run() {
+    
   while (true) {
     if (is_nvboard)
       nvboard_update();
