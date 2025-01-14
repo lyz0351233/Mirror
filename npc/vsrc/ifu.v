f0463c0acc9efbb5911af11c9da74f248c80f126
boss <boss@qq.com>
2025-01-14 21:38:02 +0800

    npc初步接入sram,支持多周期difftest

--- npc/vsrc/ifu.v
+++ npc/vsrc/ifu.v
@@ -1,4 +1,4 @@
-import "DPI-C" function int inst_fetch(input int addr);
+/* import "DPI-C" function int inst_fetch(input int addr); */
 
 module ysyx_24110006_IFU(
   input i_clock,
@@ -19,10 +19,27 @@ always@(posedge i_clock)begin
     o_valid <= 0;
   end
 end
+/* always@(posedge i_clock)begin */
+/*   if(!i_reset && !o_valid && i_valid) */
+/*     o_inst <= inst_fetch(i_pc); */
+/* end */
 
+reg ren;
 always@(posedge i_clock)begin
   if(!i_reset && !o_valid && i_valid)
-    o_inst <= inst_fetch(i_pc);
+    ren <= 1;
+  else 
+    ren <= 0;
 end
+
+ysyx_24110006_SRAM msram(
+  .i_ren(ren),
+  .i_wen(0),
+  .i_raddr(i_pc),
+  .i_waddr(0),
+  .i_wdata(0),
+  .i_wmask(0),
+  .o_rdata(o_inst)
+);
 
 endmodule
